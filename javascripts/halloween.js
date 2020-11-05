5cec81aaf421751d98098dd9316a8032b41037be
Jairo Buendía <jairobuendialopez@gmail.com>
2020-11-05 11:22:46 +0100

    v3

---
+++ javascripts/halloween.js
@@ -0,0 +1,12 @@
+function mostrar(id){
+    document.getElementById(id).style.visibility="visible"
+    }
+function ocultar(id){
+    document.getElementById(id).style.visibility="hidden"
+    }
+function sonar(id){
+    document.getElementById(id).play();
+}
+function parar(id){
+    document.getElementById(id).pause();
+}
